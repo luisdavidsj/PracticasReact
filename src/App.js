d4d97ce0c99c70d486c390a5a994ca98ef2daf62
luisdavidsj <luisdavidspider@gmail.com>
2021-07-28 20:51:48 -0500

    Estado, Propiedades y Renderizado condicional

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import logo from './logo.svg';
 import Componente from "./components/Componente";
+import Propiedades from "./components/Propiedades";
+import Estado from "./components/Estado";
+import RenderizadoCondicional from "./components/RenderizadoCondicional";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <section>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,20 +24,26 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        </section>
       <section>
         <Componente msg="Hola soy un Componente Funcional expresado desde una prop" />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <hr />
+        <Propiedades
+        cadena="Esto es una cadena de texto"
+        numero={19}
+        booleano={true}
+        arreglo={[1,2,3]}
+        objeto={{nombre:"Luis",correo:"luisdavidspider@gmail.com"}}
+        funcion={(num) => num*num}
+        elementoReact={<i>Esto es un elemento React</i>}
+        componenteReact={<Componente msg="Soy un componente pasado como Prop"/>}
+        />
+        <hr/>
+        <Estado />
+        <hr/>
+        <RenderizadoCondicional />
       </section>
+      </header>
     </div>
   );
 }
