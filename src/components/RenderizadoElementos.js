4a96c395c084ec2cab00463057aa123018eb40d8
luisdavidsj <luisdavidspider@gmail.com>
2021-07-30 00:35:02 -0500

    Renderizado de elementos

---
+++ src/components/RenderizadoElementos.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import data from '../helpers/data.json'
+
+function ElementoLista(props){
+  return(
+    <li>
+      <a href={props.el.web} rel="noreferrer" target="_blank">{props.el.name}</a>
+    </li>
+  )
+}
+
+export default class RenderizadoElementos extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      seasons:["Primavera","Verano","Otono","Invierno"],
+    };
+  }
+  render(){
+    console.log(data);
+    return(
+      <div>
+        <h2>Renderizado de elementos</h2>
+        <h3>Estaciones del año</h3>
+        <ol>
+          {
+            this.state.seasons.map((el)=>(<li key={el}>{el}</li>))
+          }
+        </ol>
+        <h3>Frameworks Frontend JavaScript</h3>
+        <ul>
+          {
+            data.frameworks.map((el)=><ElementoLista key={el.id} el={el} />)
+          }
+        </ul>
+      </div>
+    )
+  }
+}
