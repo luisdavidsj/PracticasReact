bfbb70dcb323624d99e1cd3dd9584a138470c2e8
luisdavidsj <luisdavidspider@gmail.com>
2021-08-13 14:34:49 -0500

    Hola mundo prueba

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ScrollHooks from "./components/ScrollHooks";
 import RelojHooks from "./components/RelojHooks";
 import AjaxHooks from "./components/AjaxHooks";
 import "./App.css";
+import Saludo from "./components/HolaMundo";
 
 function App() {
   return (
@@ -74,6 +75,8 @@ function App() {
           <RelojHooks />
           <hr />
           <AjaxHooks />
+          <hr />
+          <Saludo />
         </section>
       </header>
     </div>
---
+++ src/components/HolaMundo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Saludo() {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <img
+        alt="Ditto"
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+      />
+      <p>Soy Ditto!!!</p>
+    </>
+  );
+}
